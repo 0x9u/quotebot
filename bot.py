009c12ace88624f79cc1dbc43c43a58df020ed41
Oliver Po <oliver.po2882@gmail.com>
2024-12-31 15:50:59 +1000

    fix date issue

--- bot.py
+++ bot.py
@@ -14,13 +14,13 @@ import re
 
 load_dotenv()
 
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv("TOKEN")
 
-DATABASE = os.getenv('MONGO_DATABASE')
-USERNAME = os.getenv('MONGO_USERNAME')
-PASSWORD = os.getenv('MONGO_PASSWORD')
-CLUSTER = os.getenv('MONGO_CLUSTER')
-APPNAME = os.getenv('MONGO_APPNAME')
+DATABASE = os.getenv("MONGO_DATABASE")
+USERNAME = os.getenv("MONGO_USERNAME")
+PASSWORD = os.getenv("MONGO_PASSWORD")
+CLUSTER = os.getenv("MONGO_CLUSTER")
+APPNAME = os.getenv("MONGO_APPNAME")
 
 CONNECTION_STRING = f"mongodb+srv://{USERNAME}:{PASSWORD}@{CLUSTER}/?retryWrites=true&w=majority&appName={APPNAME}"
 
@@ -28,25 +28,29 @@ db = MongoClient(CONNECTION_STRING)
 scheduler = AsyncIOScheduler()
 
 australian_timezone = pytz.timezone("Australia/Sydney")
-start_date = datetime.datetime.now(australian_timezone).replace(hour=21, minute=0, second=0, microsecond=0)
+start_date = datetime.datetime.now(australian_timezone).replace(
+    hour=21, minute=0, second=0, microsecond=0
+)
 
 logging.basicConfig(level=logging.ERROR)
 
-IMAGE_REGEX = re.compile(r"(https?://[^\s]+(?:\.(?:png|jpg|jpeg|gif|webp))(?:\?[^\s]*)?)")
+IMAGE_REGEX = re.compile(
+    r"(https?://[^\s]+(?:\.(?:png|jpg|jpeg|gif|webp))(?:\?[^\s]*)?)"
+)
+
 
 class Client(discord.Client):
     def __init__(self):
         super().__init__(intents=discord.Intents.all())
         self.tree = app_commands.CommandTree(self)
 
-    
     async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
         try:
             synced = await self.tree.sync()
-            print(f'Synced {synced} commands')
+            print(f"Synced {synced} commands")
         except Exception as error:
-            print(f'Failed to sync commands, error: {error}')
+            print(f"Failed to sync commands, error: {error}")
             exit(1)
 
         scheduler.start()
@@ -58,41 +62,74 @@ class Client(discord.Client):
             channel = self.get_channel(guild["channel_id"])
             if not channel:
                 continue
-            scheduler.add_job(self.post_quote, trigger=IntervalTrigger(hours=24), start_date=start_date, args=[channel], id=str(guild["_id"]))
+            scheduler.add_job(
+                self.post_quote,
+                trigger=IntervalTrigger(hours=24),
+                start_date=start_date,
+                args=[channel],
+                id=str(guild["_id"]),
+            )
 
-    async def post_quote(self, channel: discord.TextChannel, message: discord.Message=None, use_db=True):
+    async def post_quote(
+        self, channel: discord.TextChannel, message: discord.Message = None, use_db=True
+    ):
         print("POSTING QUOTE")
         # quotes table -> guild_id, channel_id, message_id, reaction_count
-        message_id = db.get_database(DATABASE).get_collection("quotes").find_one({"_id": channel.guild.id})
+        message_id = (
+            db.get_database(DATABASE)
+            .get_collection("quotes")
+            .find_one({"_id": channel.guild.id})
+        )
         if not message_id and use_db:
             return
         message_channel = self.get_channel(message_id["channel_id"]) if use_db else None
         if not message_channel and use_db:
             return
-        message = await message_channel.fetch_message(message_id["message_id"]) if use_db else message
+        message = (
+            await message_channel.fetch_message(message_id["message_id"])
+            if use_db
+            else message
+        )
         if not message:
             return
-        embed = discord.Embed(title="Quote of the day", description=f"{message.content}\n[Jump to message]({message.jump_url})", color=discord.Color.blue())
+        embed = discord.Embed(
+            title="Quote of the day",
+            description=f"{message.content}\n[Jump to message]({message.jump_url})",
+            color=discord.Color.blue(),
+        )
         reactions = message.reactions
         print("Reactions:", reactions)
         if reactions:
             reaction_count = {reaction.emoji: reaction.count for reaction in reactions}
-            reaction_count = dict(sorted(reaction_count.items(), key=lambda item: item[1], reverse=True))
+            reaction_count = dict(
+                sorted(reaction_count.items(), key=lambda item: item[1], reverse=True)
+            )
             print(f"Reactions: {reaction_count}")
             for reaction, count in reaction_count.items():
                 embed.add_field(name=reaction, value=f"{count}", inline=True)
         else:
             print("No reactions found.")
-        embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url if message.author.avatar is not None else None)
+        embed.set_author(
+            name=message.author.display_name,
+            icon_url=(
+                message.author.avatar.url if message.author.avatar is not None else None
+            ),
+        )
         image_links = IMAGE_REGEX.findall(message.content)
-        if message.attachments and message.attachments[0].filename.endswith((".png", ".jpg", ".jpeg", ".gif")):
+        if message.attachments and message.attachments[0].filename.endswith(
+            (".png", ".jpg", ".jpeg", ".gif")
+        ):
             embed.set_image(url=message.attachments[0].url)
         elif image_links:
             embed.set_image(url=image_links[0])
         message = await channel.send(embed=embed)
 
         # check if threads are enabled
-        guild = db.get_database(DATABASE).get_collection("guilds").find_one({"_id": channel.guild.id})
+        guild = (
+            db.get_database(DATABASE)
+            .get_collection("guilds")
+            .find_one({"_id": channel.guild.id})
+        )
         if guild and guild["threads"]:
             # open thread
             try:
@@ -102,71 +139,129 @@ class Client(discord.Client):
                 print("Failed to create thread", e)
 
         if use_db:
-            db.get_database(DATABASE).get_collection("quotes").delete_one({"_id": channel.guild.id})
+            db.get_database(DATABASE).get_collection("quotes").delete_one(
+                {"_id": channel.guild.id}
+            )
+
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         if payload.user_id == self.user.id:
             return
-        
-        message = await self.get_channel(payload.channel_id).fetch_message(payload.message_id)
+
+        message = await self.get_channel(payload.channel_id).fetch_message(
+            payload.message_id
+        )
         if message.author == self.user or message.content == "":
             return
-
-        if message.created_at.day != datetime.datetime.now(datetime.timezone.utc).day:
+        message_created_time = message.created_at.replace(
+            tzinfo=datetime.timezone.utc
+        ).astimezone(australian_timezone)
+        now_time = datetime.datetime.now(australian_timezone)
+        
+        if (
+            message_created_time.day != now_time.day
+            or message_created_time.year != now_time.year
+            or message_created_time.month != now_time.month
+        ):
             return
 
         # grab quote from guild
-        quote = db.get_database(DATABASE).get_collection("quotes").find_one({"_id": message.guild.id})
-        
+        quote = (
+            db.get_database(DATABASE)
+            .get_collection("quotes")
+            .find_one({"_id": message.guild.id})
+        )
+
         # find count of greatest reaction count in message
-        reaction_count = max([reaction.count for reaction in message.reactions]) if message.reactions else 0
+        reaction_count = (
+            max([reaction.count for reaction in message.reactions])
+            if message.reactions
+            else 0
+        )
 
         if not quote or reaction_count > quote["reaction_count"]:
             if quote:
-                db.get_database(DATABASE).get_collection("quotes").delete_one({"_id": message.guild.id})
-            db.get_database(DATABASE).get_collection("quotes").insert_one({"_id": message.guild.id, "channel_id": message.channel.id, "message_id": message.id, "reaction_count": reaction_count})
-    
+                db.get_database(DATABASE).get_collection("quotes").delete_one(
+                    {"_id": message.guild.id}
+                )
+            db.get_database(DATABASE).get_collection("quotes").insert_one(
+                {
+                    "_id": message.guild.id,
+                    "channel_id": message.channel.id,
+                    "message_id": message.id,
+                    "reaction_count": reaction_count,
+                }
+            )
+
 
 bot = Client()
+
 
 @bot.tree.command(name="setup", description="Set channel to post quotes in")
 async def setup(interaction: discord.Interaction, channel: discord.TextChannel):
     await interaction.response.defer()
     if not interaction.user.guild_permissions.administrator:
-        await interaction.followup.send("You need to be an administrator to run this command", ephemeral=True)
+        await interaction.followup.send(
+            "You need to be an administrator to run this command", ephemeral=True
+        )
         return
-    
+
     if not isinstance(channel, discord.TextChannel):
         await interaction.followup.send("Invalid channel", ephemeral=True)
         return
-    
+
     if channel.guild != interaction.guild:
-        await interaction.followup.send("Channel must be in the same server as the bot", ephemeral=True)
+        await interaction.followup.send(
+            "Channel must be in the same server as the bot", ephemeral=True
+        )
         return
 
     if not channel.permissions_for(interaction.guild.me).send_messages:
-        await interaction.followup.send("I don't have permission to send messages in that channel", ephemeral=True)
+        await interaction.followup.send(
+            "I don't have permission to send messages in that channel", ephemeral=True
+        )
         return
 
-    db.get_database(DATABASE).get_collection("guilds").update_one({"_id": interaction.guild.id}, {"$set": {"channel_id": channel.id, "threads" : True }}, upsert=True)
+    db.get_database(DATABASE).get_collection("guilds").update_one(
+        {"_id": interaction.guild.id},
+        {"$set": {"channel_id": channel.id, "threads": True}},
+        upsert=True,
+    )
     await interaction.followup.send(f"Set channel to {channel.mention}")
 
     # generate a interval
-    scheduler.add_job(bot.post_quote, trigger=IntervalTrigger(hours=24), args=[channel], id=str(interaction.guild.id), start_date=start_date, replace_existing=True)
-    
+    scheduler.add_job(
+        bot.post_quote,
+        trigger=IntervalTrigger(hours=24),
+        args=[channel],
+        id=str(interaction.guild.id),
+        start_date=start_date,
+        replace_existing=True,
+    )
+
+
 @bot.tree.command(name="toggle_threads", description="Toggle threads")
 async def toggle_threads(interaction: discord.Interaction, toggle: bool):
     await interaction.response.defer()
     if not interaction.user.guild_permissions.administrator:
-        await interaction.followup.send("You need to be an administrator to run this command", ephemeral=True)
+        await interaction.followup.send(
+            "You need to be an administrator to run this command", ephemeral=True
+        )
         return
-    
-    channel = db.get_database(DATABASE).get_collection("guilds").find_one({"_id": interaction.guild.id})
+
+    channel = (
+        db.get_database(DATABASE)
+        .get_collection("guilds")
+        .find_one({"_id": interaction.guild.id})
+    )
     if not channel:
         await interaction.followup.send("Channel not set", ephemeral=True)
         return
 
-    db.get_database(DATABASE).get_collection("guilds").update_one({"_id": interaction.guild.id}, {"$set": {"threads" : toggle }}, upsert=True)
+    db.get_database(DATABASE).get_collection("guilds").update_one(
+        {"_id": interaction.guild.id}, {"$set": {"threads": toggle}}, upsert=True
+    )
     await interaction.followup.send(f"Threads set to {toggle}")
+
 
 @bot.tree.command(name="quote", description="Quote a message")
 async def quote(interaction: discord.Interaction, message_id: str):
@@ -175,7 +270,7 @@ async def quote(interaction: discord.Interaction, message_id: str):
     if not message_id.isdigit():
         await interaction.followup.send("Message not found", ephemeral=True)
         return
-    
+
     message = await interaction.channel.fetch_message(int(message_id))
     print("msg obj", message)
     print("msg type", message.type)
@@ -185,15 +280,21 @@ async def quote(interaction: discord.Interaction, message_id: str):
         return
 
     if not interaction.user.guild_permissions.administrator:
-        await interaction.followup.send("You need to be an administrator to run this command", ephemeral=True)
+        await interaction.followup.send(
+            "You need to be an administrator to run this command", ephemeral=True
+        )
         return
-    
+
     print(f"Quoting message: {message.content}")
     # if not message.content:
     #     await interaction.followup.send("Message has no content", ephemeral=True)
     #     return
 
-    channel = db.get_database(DATABASE).get_collection("guilds").find_one({"_id": interaction.guild.id})
+    channel = (
+        db.get_database(DATABASE)
+        .get_collection("guilds")
+        .find_one({"_id": interaction.guild.id})
+    )
     if not channel:
         await interaction.followup.send("Channel not set", ephemeral=True)
         return
@@ -202,15 +303,17 @@ async def quote(interaction: discord.Interaction, message_id: str):
     if not channel:
         await interaction.followup.send("Channel not found", ephemeral=True)
         return
-    
+
     if not channel.permissions_for(interaction.guild.me).send_messages:
-        await interaction.followup.send("I don't have permission to send messages in that channel", ephemeral=True)
+        await interaction.followup.send(
+            "I don't have permission to send messages in that channel", ephemeral=True
+        )
         return
-    
+
     await bot.post_quote(channel, message=message, use_db=False)
 
     await interaction.followup.send("Quoted message")
-    
+
 
 @bot.tree.command(name="force_quote", description="force scheduled quote")
 async def force_quote(interaction: discord.Interaction):
@@ -218,14 +321,20 @@ async def force_quote(interaction: discord.Interaction):
 
     # check if user is admin
     if not interaction.user.guild_permissions.administrator:
-        await interaction.followup.send("You need to be an administrator to run this command", ephemeral=True)
+        await interaction.followup.send(
+            "You need to be an administrator to run this command", ephemeral=True
+        )
         return
-    
-    channel = db.get_database(DATABASE).get_collection("guilds").find_one({"_id": interaction.guild.id})
+
+    channel = (
+        db.get_database(DATABASE)
+        .get_collection("guilds")
+        .find_one({"_id": interaction.guild.id})
+    )
     if not channel:
         await interaction.followup.send("Channel not set", ephemeral=True)
         return
-    
+
     channel = bot.get_channel(channel["channel_id"])
     if not channel:
         await interaction.followup.send("Channel not found", ephemeral=True)
@@ -235,14 +344,21 @@ async def force_quote(interaction: discord.Interaction):
 
     await interaction.followup.send("Forced quote")
 
+
 @bot.tree.command(name="debug_schedule", description="schedule quote")
 async def debug_schedule(interaction: discord.Interaction, seconds: int):
     await interaction.response.defer()
     if not interaction.user.guild_permissions.administrator:
-        await interaction.followup.send("You need to be an administrator to run this command", ephemeral=True)
+        await interaction.followup.send(
+            "You need to be an administrator to run this command", ephemeral=True
+        )
         return
     # get channel
-    channel = db.get_database(DATABASE).get_collection("guilds").find_one({"_id": interaction.guild.id})
+    channel = (
+        db.get_database(DATABASE)
+        .get_collection("guilds")
+        .find_one({"_id": interaction.guild.id})
+    )
     if not channel:
         await interaction.followup.send("Channel not set", ephemeral=True)
         return
@@ -251,7 +367,7 @@ async def debug_schedule(interaction: discord.Interaction, seconds: int):
     if not channel:
         await interaction.followup.send("Channel not found", ephemeral=True)
         return
-    
+
     scheduler.add_job(bot.post_quote, CronTrigger(second=seconds), args=[channel])
 
     await interaction.followup.send("Scheduled quote")
@@ -263,6 +379,7 @@ def handle_exception(exc_type, exc_value, exc_traceback):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
     logging.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
 
 # Override the default exception handler
 sys.excepthook = handle_exception
